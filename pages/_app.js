ee76279cffe8066f684ddf4d14e050f96ad19989
hassanamjad1 <haxxter1977@gmail.com>
2021-07-29 08:58:51 -0400

    Adding ApolloClient and TailwindCSSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
-import '../styles/globals.css';
+import '../styles/globals.css'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import 'tailwindcss/tailwind.css'
 
-
-;
-
+const client = new ApolloClient({
+  uri : 'https://demo.saleor.io/graphql/',
+  cache : new InMemoryCache()
+})
 
 
 
 function MyApp({ Component, pageProps }) {
-  return -1
-    
+  return (0)    
 }
 
 export default MyApp
